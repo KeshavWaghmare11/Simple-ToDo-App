42ccd732a36efda13302db6d2c1145151308c236
KeshavWaghmare11 <114676895+KeshavWaghmare11@users.noreply.github.com>
2022-10-02 16:16:27 +0530

    Add files via upload

---
+++ config/constants.php
@@ -0,0 +1,12 @@
+<?php 
+
+session_start();
+
+define('LOCALHOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'task_manager');
+
+define('SITEURL', 'http://localhost/Simple-ToDo-App-main/');
+
+?> 
